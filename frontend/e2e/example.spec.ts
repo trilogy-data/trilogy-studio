eb7c11beb668d2a2de1827d6b2cb88a6ba9cd204
Ethan Dickinson <ethan.dickinson@gmail.com>
2024-04-17 08:43:11 -0400

    working frontend test

--- frontend/e2e/example.spec.ts
+++ frontend/e2e/example.spec.ts
@@ -4,6 +4,6 @@ import { _electron } from 'playwright';
 test('App launches and quits', async () => {
   const app = await _electron.launch({args: ['dist-electron/main.js']});
   const window = await app.firstWindow();
-  await expect(await window.title()).toContain('Loading');
+  await expect(await window.title()).toContain('PreQL Studio');
   await app.close();
 });
